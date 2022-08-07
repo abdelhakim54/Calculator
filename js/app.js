c6fb53d0eb64b68eaa2168ae72c60cf37f3bfa65
Abdelhakim <elouakouakabdelhakim@gmail.com>
2022-08-07 12:40:20 +0100

    add: operate() function
    * operate() function takes an operator and 2 numbers and calls the
    specific operator on the 2 numbers

--- js/app.js
+++ js/app.js
@@ -13,3 +13,10 @@ function multiply(a, b){
 function divide(a, b){
     return b == 0 ? "ERROR: cannot divide by zero":a/b ;
 }
+
+function operate(operator, first, second){
+    if(operator == "add") return add(first, second);
+    else if(operator == "multiply") return multiply(first, second);
+    else if(operator == "subtract") return subtract(first, second);
+    else if(operator == "divide") return divide(first, second);
+}
