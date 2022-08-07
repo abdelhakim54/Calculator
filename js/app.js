5d04c07034cfed628d8c57c99c7b60bcacc646a2
Abdelhakim <elouakouakabdelhakim@gmail.com>
2022-08-07 19:58:12 +0100

    add: eventListeners to buttons
    * add events eventListeners to all buttons except the equal button

--- js/app.js
+++ js/app.js
@@ -20,3 +20,19 @@ function operate(operator, first, second){
     else if(operator == "subtract") return subtract(first, second);
     else if(operator == "divide") return divide(first, second);
 }
+
+const buttons = document.querySelectorAll(".button");
+const display = document.querySelector(".display")
+
+const ac = document.querySelector(".clear-all");
+const c = document.querySelector(".clear");
+
+ac.addEventListener("click", () => display.textContent = "");
+c.addEventListener("click", () => display.textContent = display.textContent.slice(0, -1));
+
+buttons.forEach(button => {
+    button.addEventListener("click", function(e){
+        if(display.textContent.trim() == '0')display.textContent = "";
+        display.textContent += e.target.textContent ;
+        })
+})
